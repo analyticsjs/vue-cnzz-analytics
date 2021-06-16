45243713a6c49ae852de1cb65d0bb37a6e424230
程沛权 <chengpeiquan@chengpeiquan.com>
2021-06-16 14:00:30 +0800

    release: v2.2.0

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,5 +1,15 @@
+import type { Options, Vue } from '@/types'
 export declare function usePush(): {
-    pv: (pageUrl: string, fromUrl?: string) => void;
-    event: (category: string, action: string, label: string, value: number, nodeId: string) => void;
-};
-export default function install(Vue: Vue, { router, siteIdList, isDebug }: Partial<Options>): boolean;
+  pv: (pageUrl: string, fromUrl?: string) => void
+  event: (
+    category: string,
+    action: string,
+    label: string,
+    value: number,
+    nodeId: string
+  ) => void
+}
+export default function install(
+  Vue: Vue,
+  { router, siteIdList, isDebug }: Partial<Options>
+): boolean
